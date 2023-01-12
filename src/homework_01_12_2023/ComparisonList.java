c08a79888e0a04b291752319be3c99350cc9cfc9
Konstantin Bezgatsev <gelandewagen1@gmail.com>
2023-01-12 19:50:52 +0100

    Initial commit

---
+++ src/homework_01_12_2023/ComparisonList.java
@@ -0,0 +1,42 @@
+package homework_01_12_2023;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class ComparisonList {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        for (int i = 0; i < 100000; i++) {
+            arrayList.add(i);
+            linkedList.add(i);
+        }
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < arrayList.size(); i++)
+            arrayList.get(i);
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken for arrayList is " + (start) + " ms");
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < linkedList.size(); i++)
+            linkedList.get(i);
+        end = System.currentTimeMillis();
+        System.out.println("Time taken for linkedList is " + (end - start) + " ms");
+
+        start = System.currentTimeMillis();
+        Iterator<Integer> iterator = linkedList.iterator();
+        while (iterator.hasNext())
+            iterator.next();
+        end = System.currentTimeMillis();
+        System.out.println("Time taken for Iterator linkedList is " + (end - start) + " ms");
+    }
+}
+
+// ArrayList знает адрес и по индексу быстро находит. O(n)
+// LinkedList всегда начинает поиск с нулевого элемента. Медленнее. O(n^2)
+// Для LinkedList лучше использовать Iterator.
---
+++ src/homework_01_12_2023/DynamicArray.java
@@ -0,0 +1,206 @@
+package homework_01_12_2023;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class DynamicArray implements Iterable<Integer> {
+
+    private int[] array;
+    private int size;
+    private int count;
+
+    DynamicArray() {
+        array = new int[1];
+        size = 1;
+        count = 0;
+    }
+
+    DynamicArray(int capacity) {
+        array = new int[capacity];
+        size = capacity;
+        count = 0;
+    }
+
+    public void add(int input) {
+        if (count >= size) {
+            growSize();
+        }
+        array[count++] = input;
+    }
+
+    private void growSize() {
+        int[] newArray = new int[size * 2];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+        size = array.length;
+    }
+
+    public void growSize(int capacity) {
+        int[] newArray = new int[capacity];
+        for (int i = 0; i < count; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+        size = array.length;
+    }
+
+    public void addAt(int index, int element) {
+        if (index >= size - 1) {
+            growSize(index + 1);
+            array[index] = element;
+            count = index + 1;
+        } else {
+            for (int i = count; i > index; i--) {
+                array[i] = array[i - 1];
+            }
+            array[index] = element;
+            count++;
+        }
+    }
+
+    public void remove() {
+        if (count > 0) {
+//            array[count - 1] = 0;
+            count--;
+        }
+    }
+
+    public void removeAt(int index) {
+        if (index < count) {
+            for (int i = index; i < count - 1; i++) {
+                array[i] = array[i + 1];
+            }
+//            array[count - 1] = 0;
+            count--;
+        }
+    }
+
+    public void shrinkSize() {
+        if (count < size) {
+            int[] newArray = new int[count];
+            for (int i = 0; i < count; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+            size = count;
+        }
+    }
+
+    public String toString() {
+
+        String out = "[";
+        for (int i = 0; i < count; i++) {
+            out = out + " " + array[i];
+        }
+        out = out + "]";
+        return out;
+    }
+
+    public void set(int index, int element) {
+        if (index >= count - 1) {
+            addAt(index, element);
+        } else {
+            array[index] = element;
+        }
+    }
+
+    public int get(int index) {
+        if (index >= count - 1) throw new ArrayIndexOutOfBoundsException();
+        return array[index];
+    }
+
+    public void clear() {
+        count = 0;
+    }
+
+
+    public static void main(String[] args) {
+
+//        DynamicArray<String> dynamicArray = new DynamicArray<>();
+//        DynamicArray<Object> objectDynamicArray = new DynamicArray<>();
+
+        DynamicArray dynamicArray = new DynamicArray();
+
+        System.out.println(Arrays.toString(dynamicArray.array));
+        dynamicArray.add(1);
+        System.out.println(Arrays.toString(dynamicArray.array));
+        dynamicArray.add(2);
+        System.out.println(Arrays.toString(dynamicArray.array));
+        dynamicArray.add(3);
+        System.out.println(Arrays.toString(dynamicArray.array));
+        dynamicArray.add(4);
+        System.out.println(Arrays.toString(dynamicArray.array));
+        dynamicArray.add(5);
+        System.out.println(Arrays.toString(dynamicArray.array));
+
+        dynamicArray.addAt(5, 6);
+        System.out.println(Arrays.toString(dynamicArray.array));
+        System.out.println(dynamicArray.size);
+        System.out.println(dynamicArray.count);
+
+        dynamicArray.remove();
+        System.out.println(Arrays.toString(dynamicArray.array));
+        System.out.println(dynamicArray.size);
+        System.out.println(dynamicArray.count);
+
+        dynamicArray.removeAt(0);
+        System.out.println(Arrays.toString(dynamicArray.array));
+        System.out.println(dynamicArray);
+
+        dynamicArray.remove();
+        System.out.println(dynamicArray);
+        System.out.println(Arrays.toString(dynamicArray.array));
+
+        dynamicArray.shrinkSize();
+        System.out.println(dynamicArray);
+        System.out.println(Arrays.toString(dynamicArray.array));
+
+        dynamicArray.set(0, 10);
+        System.out.println(dynamicArray);
+        dynamicArray.set(10, 10);
+        System.out.println(dynamicArray);
+        System.out.println(dynamicArray.get(0));
+
+//        dynamicArray.clear();
+//        System.out.println(dynamicArray);
+//        System.out.println(Arrays.toString(dynamicArray.array));
+
+        boolean contains = dynamicArray.contains(10);
+        System.out.println(contains);
+
+        boolean isEmpty = dynamicArray.isEmpty();
+        System.out.println(isEmpty);
+
+        dynamicArray.forEach(System.out::println);
+    }
+
+    public boolean contains(int data) {
+        for (int i = 0; i < count; i++) {
+            if (data == array[i])
+                return true;
+        }
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            int i = 0;
+
+            @Override
+            public boolean hasNext() {
+                return i < count;
+            }
+
+            @Override
+            public Integer next() {
+                return array[i++];
+            }
+        };
+    }
+}
