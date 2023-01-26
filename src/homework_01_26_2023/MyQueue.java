78703e693da93b15bd31d3a8dd0f7b42219e2303
Konstantin Bezgatsev <gelandewagen1@gmail.com>
2023-01-26 18:11:49 +0100

    Initial commit

---
+++ src/homework_01_26_2023/MyQueue.java
@@ -0,0 +1,75 @@
+package homework_01_26_2023;
+
+public class MyQueue {
+    private int[] arr;      // массив для хранения элементов queue
+    private int head;      // head указывает на передний элемент в queue
+    private int tail;       // tail часть указывает на последний элемент в queue
+    private int capacity;   // максимальная емкость queue
+    private int count;      // текущий размер queue
+
+    public MyQueue(int size) {
+        arr = new int[size];
+        capacity = size;
+        head = 0;
+        tail = -1;
+        count = 0;
+    }
+
+    public boolean pushToEnd(int data) {
+        boolean res = false;
+        if (count < capacity) {
+            arr[count] = data;
+            count++;
+            tail++;
+            res = true;
+        }
+        return res;
+    }
+
+    public int remove() {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("The queue is empty!!!");
+        }
+        int val = arr[head];
+        for (int i = 1; i < count; i++) {
+            arr[i - 1] = arr[i];
+        }
+        arr[tail] = 0;
+        count--;
+        tail--;
+        return val;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("The queue is empty!!!");
+        }
+        return arr[head];
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public static void main(String[] args) {
+        MyQueue q = new MyQueue(5);
+        System.out.println(q.pushToEnd(1));
+        System.out.println(q.pushToEnd(2));
+        System.out.println(q.pushToEnd(3));
+        System.out.println(q.pushToEnd(4));
+        System.out.println(q.pushToEnd(5));
+        System.out.println("Peek: " + q.peek());
+
+        System.out.println("Size: " + q.size());
+
+        System.out.println("Remove: " + q.remove());
+        System.out.println("Remove: " + q.remove());
+
+        System.out.println("Size: " + q.size());
+    }
+}
+
---
+++ src/homework_01_26_2023/MyStack.java
@@ -0,0 +1,80 @@
+package homework_01_26_2023;
+
+import java.util.Stack;
+
+public class MyStack {
+
+    private int[] array;
+    private int count;
+
+    public MyStack(int size) {
+        array = new int[size];
+        count = 0;
+    }
+
+    public void push(int element) {
+        if (count == array.length) throw new RuntimeException("Stack is full");
+        array[count++] = element;
+    }
+
+    public int pop() {
+        if (count == 0) throw new RuntimeException("Stack is empty");
+        return array[--count];
+    }
+
+    public int peek() {
+        if (count == 0) throw new RuntimeException("Stack is empty");
+        return array[count - 1];
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    public int search(int element) {
+        int countOfNum = count + 1;
+        int index = -1;
+        if (!isEmpty()) {
+            for (int i = 0; i < count; i++) {
+                countOfNum--;
+                if (element == array[i]) {
+                    index = countOfNum;
+                    break;
+                }
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+
+        MyStack myStack = new MyStack(5);
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(4);
+        myStack.push(5);
+        System.out.println(myStack.isEmpty());
+        System.out.println("peek: " + myStack.peek());
+
+        int num1 = 4;
+        int num2 = 2;
+        int num3 = 1;
+        int num4 = 0;
+        System.out.println("Position from peek in stack of number \"" + num1 + "\": " + myStack.search(num1));
+        System.out.println("Position from peek in stack of number \"" + num2 + "\": " + myStack.search(num2));
+        System.out.println("Position from peek in stack of number \"" + num3 + "\": " + myStack.search(num3));
+        System.out.println("Position from peek in stack of number \"" + num4 + "\": " + myStack.search(num4));
+
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.isEmpty());
+        System.out.println("Position from peek in stack of number \"" + num1 + "\": " + myStack.search(num1));
+        System.out.println("Position from peek in stack of number \"" + num2 + "\": " + myStack.search(num2));
+    }
+}
+
+
